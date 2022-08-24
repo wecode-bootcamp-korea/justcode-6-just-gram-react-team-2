621bf89505607a45627a2f5d1c3db30716443acd
이고운 <kowoon@igoun-ui-MacBookAir.local>
2022-08-24 18:39:30 +0900

    REV: 인증 확인하기

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,9 +2,14 @@ import React, { useEffect } from "react";
 import "./Main.scss";
 import Feed from "./Feed";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 function Main() {
   const [feeds, setFeeds] = useState([]);
+  const [getUser, setGetUser] = useState();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("./data/feeds.json")
@@ -13,6 +18,23 @@ function Main() {
         // console.log(data.feedData);
         setFeeds(data.feedData);
       });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://auth.jaejun.me:10010/me", {
+      method: "GET",
+      headers: {
+        Authorization:
+          // "hi",
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NiwiaWF0IjoxNjYxMjY4MjcwLCJleHAiOjE2NjM5NDY2NzB9.i32_BYRvoN4gxzBcwT5dXAhccbDKnKn85hUmcPgnlHQ",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        data.email !== undefined ? setGetUser(data) : navigate("/");
+      });
+    // console.log(data.email); 이메일 찍히는지 보기
+    // 삼항연산자로 조건 부여하기
   }, []);
 
   return (
@@ -42,11 +64,14 @@ function Main() {
             alt="하트"
             src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
           />
-          <img
+
+          <p>{getUser && getUser.email}</p>
+          {/* //이메일 있을 때만 렌더링 할 거니까 조건부 렌더링 &&로 넣어줘야함. */}
+          {/* <img
             className="icon-img custom-margin-left"
             alt="사람"
             src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
-          />
+          /> */}
         </div>
       </div>
       <div className="flex-center">
